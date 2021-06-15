4d7a917feb02a2b875b4729881506930d2395e68
Jesus Alvarez Ruiz <jalvarezruiz@gmail.com>
2021-06-15 12:19:32 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+basic.showString("Brujula")
+input.calibrateCompass()
+basic.forever(function () {
+    if (input.compassHeading() < 22.5) {
+        basic.showArrow(ArrowNames.North)
+    } else if (input.compassHeading() < 67.5) {
+        basic.showArrow(ArrowNames.NorthEast)
+    } else if (input.compassHeading() < 112.5) {
+        basic.showArrow(ArrowNames.East)
+    } else if (input.compassHeading() < 157.5) {
+        basic.showArrow(ArrowNames.SouthEast)
+    } else if (input.compassHeading() < 202.5) {
+        basic.showArrow(ArrowNames.South)
+    } else if (input.compassHeading() < 247.5) {
+        basic.showArrow(ArrowNames.SouthWest)
+    } else if (input.compassHeading() < 292.5) {
+        basic.showArrow(ArrowNames.West)
+    } else if (input.compassHeading() < 337.5) {
+        basic.showArrow(ArrowNames.NorthWest)
+    } else {
+        basic.showArrow(ArrowNames.North)
+    }
+})
